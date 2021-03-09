bf6e525ef4d230b971ce028433ac630967779af9
nishith-epam <79639642+nishith-epam@users.noreply.github.com>
2021-03-09 15:57:19 +0530

    Thirdparty library added for material design and bootstrap

---
+++ day-9/third-party-material-project/src/app/material-module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  exports: [
+   
+    MatButtonModule,
+   
+   
+  ]
+})
+export class DemoMaterialModule {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
